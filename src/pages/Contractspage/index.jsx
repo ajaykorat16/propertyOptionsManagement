1dc48b0904db4d92e9af96403e7e2c60df1e0aa9
Ajay Korat <ajaykorat16@gmail.com>
2023-12-05 11:27:11 +0530

    Manage responsive design

--- src/pages/Contractspage/index.jsx
+++ src/pages/Contractspage/index.jsx
@@ -67,68 +67,73 @@ const ContractspagePage = () => {
               <Sidebar2 className="!w-[232px] bg-gray-900_03 flex inset-[0] justify-center overflow-auto" />
             </div>
             <div className="bg-white-A700 flex flex-col mainFinisherWrapper font-montserrat items-center justify-start p-10 md:px-5 w-[83%] ">
-              <Text
-                className="md:ml-[0] mt-[60px] text-center md:text-3xl sm:text-[28px] text-[32px] text-gray-900"
-                size="txtMontserratRomanSemiBold32"
-              >
-                Contracts
-              </Text>
-              <div className="flex md:gap-5 items-center justify-between mt-[76px] w-full sm:flex-col">
-                <div className="flex items-center justify-between sm:flex-col items-baseline">
-                  <Text
-                    className="text-base text-gray-900_03 mr-[16px]"
-                    size="txtMontserratRomanSemiBold16Gray90003"
-                  >
-                    Filter
-                  </Text>
-                  <Dropdown value={filter} options={filterOptionList} onChange={(e) => setFilter(e.target.value)}
-                    className="rounded-md text-xs bg-fill text-white_A700 border border-gray-500_7f shadow-bs  border-solid text-base text-left w-[307px] h-[40px]" />
+              <div className="flex flex-col justify-start mb-[159px] mt-[18px] w-full">
+                <Text
+                  className="md:ml-[0] text-center md:text-3xl sm:text-[28px] text-[32px] text-gray-900"
+                  size="txtMontserratRomanSemiBold32"
+                >
+                  Contracts
+                </Text>
+                <div className="flex md:gap-5 items-center justify-between mt-[76px] w-full sm:flex-col">
+                  <div className="flex items-center justify-between sm:flex-col items-baseline">
+                    <Text
+                      className="text-base text-gray-900_03 mr-[16px]"
+                      size="txtMontserratRomanSemiBold16Gray90003"
+                    >
+                      Filter
+                    </Text>
+                    <Dropdown value={filter} options={filterOptionList} onChange={(e) => setFilter(e.target.value)}
+                      className="rounded-md text-xs bg-fill text-white_A700 border border-gray-500_7f shadow-bs  border-solid text-base text-left w-[307px] h-[40px]" />
+                  </div>
+                  <div className="flex items-center justify-between sm:flex-col items-baseline">
+                    <Text
+                      className="text-base text-gray-900_03 w-[59px] mr-[8px]"
+                      size="txtMontserratRomanSemiBold16Gray90003"
+                    >
+                      Sort by
+                    </Text>
+                    <Dropdown value={sortBy} options={sortOptionList} onChange={(e) => setSortBy(e.target.value)}
+                      className="rounded-md text-xs bg-fill text-white_A700 border border-gray-500_7f shadow-bs  border-solid text-base text-left w-[307px] h-[40px]" />
+                  </div>
                 </div>
-                <div className="flex items-center justify-between sm:flex-col items-baseline">
-                  <Text
-                    className="text-base text-gray-900_03 w-[59px] mr-[8px]"
-                    size="txtMontserratRomanSemiBold16Gray90003"
-                  >
-                    Sort by
-                  </Text>
-                  <Dropdown value={sortBy} options={sortOptionList} onChange={(e) => setSortBy(e.target.value)}
-                    className="rounded-md text-xs bg-fill text-white_A700 border border-gray-500_7f shadow-bs  border-solid text-base text-left w-[307px] h-[40px]" />
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-start mb-[329px] mt-[48px] w-[98%] md:w-full">
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 grid-cols-6 justify-center w-full gap-x-4 gap-y-6 md:gap-5">
-                  {contarcts.length !== 0 ? (
-                    contarcts.map((c) => (
-                      <div key={c.id} className="flex flex-1 flex-col items-center justify-start w-full md:w-1/2"
-                        style={c.isRead === 1 ? ({ opacity: 0.7 }) : null}
-                        onClick={() => navigate(`/dashboard/contractfullview/${c._id}`)}
-                      >
-                        <div className="flex flex-row justify-evenly cursor-pointer">
-                          <Img
-                            className="h-14 w-14"
-                            src="/images/img_lafilecontract.svg"
-                            alt="lafilecontract"
-                          />
-                          <div className="contarct-text flex flex-col items-start justify-start">
-                            <Text
-                              className="contract-text-id gap-y-4 text-gray-900_03 w-[87px]"
-                              size="txtMontserratRomanRegular16Gray90003"
-                            >
-                              {`ID ${c.id}`}
-                            </Text>
-                            <Text
-                              className="contract-text-date text-gray-900_01 w-[87px]"
-                              size="txtMontserratRomanRegular16"
-                            >
-                              {c.createdAt}
-                            </Text>
+                <div className=" flex flex-col items-center justify-start">
+                  <div className="inner-container flex flex-col gap-4 items-center " orientation="vertical">
+                    <div className="items-container">
+                      {contarcts.length !== 0 ? (
+                        contarcts.map((c) => (
+                          <div key={c.id}
+                            className="item singleFinisherItem flex flex-col items-center justify-start"
+                            style={c.isRead === 1 ? ({ opacity: 0.7 }) : null}
+                            onClick={() => navigate(`/dashboard/contractfullview/${c._id}`)}
+                          >
+                            <div className="flex flex-row justify-evenly cursor-pointer">
+                              <Img
+                                className="h-14 w-14"
+                                src="/images/img_lafilecontract.svg"
+                                alt="lafilecontract"
+                              />
+                              <div className="contarct-text flex flex-col items-start justify-start">
+                                <Text
+                                  className="contract-text-id gap-y-4 text-gray-900_03 w-[87px]"
+                                  size="txtMontserratRomanRegular16Gray90003"
+                                >
+                                  {`ID ${c.id}`}
+                                </Text>
+                                <Text
+                                  className="contract-text-date text-gray-900_01 w-[87px]"
+                                  size="txtMontserratRomanRegular16"
+                                >
+                                  {c.createdAt}
+                                </Text>
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                    ))
-                  ) : (
-                    "Contacts Not Found"
-                  )}
+                        ))
+                      ) : (
+                        "Contacts Not Found"
+                      )}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
